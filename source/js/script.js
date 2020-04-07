e84f13a3896d76a40334b902b8ad1f6b92692bd3
vetneka <44982805+vetneka@users.noreply.github.com>
2020-04-08 00:08:27 +0400

    fix(keyboard) - fix styles

--- source/js/script.js
+++ source/js/script.js
@@ -120,7 +120,11 @@ const runsOnKeys = function (func, keys) {
 };
 
 window.addEventListener('load', function () {
-  currentLang = localStorage.getItem('currentLang');
+  setInfo();
+
+  if (localStorage.getItem('currentLang')) {
+    currentLang = localStorage.getItem('currentLang');
+  }
 
   const textarea = createTextarea();
   document.body.appendChild(textarea);
@@ -333,3 +337,14 @@ const renderKeyboard = function (lang) {
 
   keyboardButtons = document.querySelectorAll('.keyboard__key');
 }
+
+const setInfo = function () {
+  let title = document.createElement('h1');
+  title.classList.add('info__title');
+  title.textContent = 'RSS Virtual Keyboard';
+
+  let info = document.createElement('p');
+  info.classList.add('info__description');
+  info.textContent = 'Клавиатура создана в операционной системе Windows. Для переключения языка комбинация: левыe Shift + Alt';
+  document.body.append(title, info);
+};
